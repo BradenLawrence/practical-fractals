ee5762a4d1ca710efb4c44b81a8986c7abc8e0c5
blip <b@bradenlawrence.com>
2018-12-13 21:42:27 -0500

    add canvas

--- app/components/display-area.js
+++ app/components/display-area.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  didInsertElement() {
+    this.initCanvas()
+  },
+  initCanvas() {
+    const canvas = this.element.querySelector('canvas')
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'green';
+    ctx.fillRect(10, 10, 150, 100);
+  }
 });
