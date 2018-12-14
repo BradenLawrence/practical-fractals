a7d6e49bc279272fb751d2e877c2bb7f2bf0c5a1
blip <b@bradenlawrence.com>
2018-12-13 23:18:14 -0500

    Fullscreen canvas

--- app/components/display-area.js
+++ app/components/display-area.js
@@ -1,17 +1,18 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  screenWidth:  document.documentElement.clientWidth,
+  screenHeight:  document.documentElement.clientHeight,
   didInsertElement() {
     this.initCanvas()
   },
   initCanvas() {
     const canvas = this.element.querySelector('canvas')
     const ctx = canvas.getContext('2d');
-    const size = 10;
-    // ctx.fillRect(10, 10, 150, 100);
+    const size = 1;
     for(let x=0; x<canvas.width; x+=size){
       for(let y=0; y<canvas.height; y+=size){
-        ctx.fillStyle = `rgb(${x},${y},100)`;
+        ctx.fillStyle = `rgb(${x/2},${y/2},100)`;
         ctx.fillRect(x,y,size,size)
       }
     }
