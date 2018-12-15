97cea49edb2f31c985feae04994f9dc02ef8d64e
blip <b@bradenlawrence.com>
2018-12-15 12:53:08 -0500

    add precision and permissiveness sliders

--- app/components/display-area.js
+++ app/components/display-area.js
@@ -1,15 +1,15 @@
 import Component from '@ember/component';
 
-const mandelBrotChecker = (x,y) => {
+const mandelBrotChecker = (x,y, p) => {
   let real = x;
   let imaginary = y;
-  for(let i=0; i<20; i++){
+  for(let i=0; i<p; i++){
     let nextReal = real*real-imaginary*imaginary+x;
     let nextImaginary = 2*real*imaginary+y;
     real = nextReal;
     imaginary = nextImaginary;
   }
-  if(real*imaginary<5){
+  if(real*imaginary<2){
     return true;
   } else {
     return false;
@@ -19,26 +19,40 @@ const mandelBrotChecker = (x,y) => {
 export default Component.extend({
   screenWidth:  document.documentElement.clientWidth,
   screenHeight:  document.documentElement.clientHeight,
+  size: 10,
+  permissiveness: 100,
   didInsertElement() {
     this.initCanvas()
   },
   initCanvas() {
     const canvas = this.element.querySelector('canvas')
     const ctx = canvas.getContext('2d');
-    const size = 2;
-    const zoom = 200;
-    const panX = 5;
-    const panY = 2.2;
-    for(let x=0; x<canvas.width; x+=size){
-      for(let y=0; y<canvas.height; y+=size){
-        const doesBelong = mandelBrotChecker(x/zoom-panX,y/zoom-panY)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    const sizeCounter = Number(this.size);
+    const permissivenessCounter = Number(this.permissiveness)
+    const zoom = 300;
+    const panX = 3;
+    const panY = 1.5;
+    for(let x=0; x<canvas.width; x+=sizeCounter){
+      for(let y=0; y<canvas.height; y+=sizeCounter){
+        const doesBelong = mandelBrotChecker(x/zoom-panX,y/zoom-panY, permissivenessCounter)
         if(doesBelong){
           ctx.fillStyle = 'black';
         } else {
           ctx.fillStyle = 'blue';
         }
-        ctx.fillRect(x,y,size,size)
+        ctx.fillRect(x,y,sizeCounter,sizeCounter)
       }
+    }
+  },
+  actions: {
+    sliderUpdate(value) {
+      this.initCanvas();
+      return value;
+    },
+    permissivenessUpdate(value) {
+      this.initCanvas();
+      return value;
     }
   }
 });
