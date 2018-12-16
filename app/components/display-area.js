891cd45ae570c1bbe239eb8253f6abdad60970fb
blip <b@bradenlawrence.com>
2018-12-15 19:50:18 -0500

    Move display settings to controller

--- app/components/display-area.js
+++ app/components/display-area.js
@@ -1,21 +1,5 @@
 import Component from '@ember/component';
 
-const mandelBrotChecker = (x,y, p) => {
-  let real = x;
-  let imaginary = y;
-  for(let i=0; i<p; i++){
-    let nextReal = real*real-imaginary*imaginary+x;
-    let nextImaginary = 2*real*imaginary+y;
-    real = nextReal;
-    imaginary = nextImaginary;
-  }
-  if(real*imaginary<2){
-    return true;
-  } else {
-    return false;
-  }
-}
-
 export default Component.extend({
   screenWidth:  document.documentElement.clientWidth,
   screenHeight:  document.documentElement.clientHeight,
@@ -28,25 +12,25 @@ export default Component.extend({
     const canvas = this.element.querySelector('canvas')
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    const sizeCounter = Number(this.size);
-    const permissivenessCounter = Number(this.permissiveness)
-    const zoom = 300;
-    const panX = 3;
-    const panY = 1.5;
+    const sizeCounter = Number(this.precision.blockSize);
+    const permissivenessCounter = Number(this.precision.permissiveness)
+    const panX = this.position.x;
+    const panY = this.position.y;
+    const zoom = this.position.zoom;
     for(let x=0; x<canvas.width; x+=sizeCounter){
       for(let y=0; y<canvas.height; y+=sizeCounter){
-        const doesBelong = mandelBrotChecker(x/zoom-panX,y/zoom-panY, permissivenessCounter)
+        const doesBelong = this.formula.mandelbrot.check(x/zoom-panX,y/zoom-panY, permissivenessCounter)
         if(doesBelong){
-          ctx.fillStyle = 'black';
+          ctx.fillStyle = this.color.belongsColor;
         } else {
-          ctx.fillStyle = 'blue';
+          ctx.fillStyle = this.color.failColor1;
         }
         ctx.fillRect(x,y,sizeCounter,sizeCounter)
       }
     }
   },
   actions: {
-    sliderUpdate(value) {
+    sizeUpdate(value) {
       this.initCanvas();
       return value;
     },
