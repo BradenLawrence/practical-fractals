df50a126a6915f3f5ea9e41275586896598c4f04
blip <b@bradenlawrence.com>
2018-12-15 19:49:45 -0500

    rename controller from fractal.js to application.js

---
+++ app/controllers/application.js
@@ -0,0 +1,36 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  color: {
+    belongsColor: '#000000',
+    failColor1:  '#0000FF'
+  },
+  formula: {
+    mandelbrot: {
+      check(x,y,permissiveness){
+        let real = x;
+        let imaginary = y;
+        for(let i=0; i<permissiveness; i++){
+          let nextReal = real*real-imaginary*imaginary+x;
+          let nextImaginary = 2*real*imaginary+y;
+          real = nextReal;
+          imaginary = nextImaginary;
+        }
+        if(real*imaginary<2){
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  },
+  position: {
+    x: 3,
+    y: 1.5,
+    zoom: 300
+  },
+  precision: {
+    blockSize: 5,
+    permissiveness: 50
+  }
+});
