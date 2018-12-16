880249624fc096f430920ad5f5ccfd6c6b765de4
blip <b@bradenlawrence.com>
2018-12-15 23:56:48 -0500

    Move precision slider logic into a child component of the display

--- app/components/display-area.js
+++ app/components/display-area.js
@@ -4,36 +4,28 @@ export default Component.extend({
   screenWidth:  document.documentElement.clientWidth,
   screenHeight:  document.documentElement.clientHeight,
   didInsertElement() {
-    this.initCanvas()
-  },
-  initCanvas() {
-    const canvas = this.element.querySelector('canvas')
-    const ctx = canvas.getContext('2d');
-    const sizeCounter = Number(this.precision.blockSize);
-    const permissivenessCounter = Number(this.precision.permissiveness)
-    const panX = this.position.x;
-    const panY = this.position.y;
-    const zoom = this.position.zoom;
-    for(let x=0; x<canvas.width; x+=sizeCounter){
-      for(let y=0; y<canvas.height; y+=sizeCounter){
-        const doesBelong = this.formula.mandelbrot.check(x/zoom-panX,y/zoom-panY, permissivenessCounter)
-        if(doesBelong){
-          ctx.fillStyle = this.color.belongsColor;
-        } else {
-          ctx.fillStyle = this.color.failColor1;
-        }
-        ctx.fillRect(x,y,sizeCounter,sizeCounter)
-      }
-    }
+    this.send('initCanvas');
   },
   actions: {
-    sizeUpdate(value) {
-      this.initCanvas();
-      return value;
-    },
-    permissivenessUpdate(value) {
-      this.initCanvas();
-      return value;
-    }
+    initCanvas() {
+      const canvas = this.element.querySelector('canvas')
+      const ctx = canvas.getContext('2d');
+      const sizeCounter = Number(this.precision.blockSize);
+      const permissivenessCounter = Number(this.precision.permissiveness)
+      const panX = this.position.x;
+      const panY = this.position.y;
+      const zoom = this.position.zoom;
+      for(let x=0; x<canvas.width; x+=sizeCounter){
+        for(let y=0; y<canvas.height; y+=sizeCounter){
+          const doesBelong = this.formula.mandelbrot.check(x/zoom-panX,y/zoom-panY, permissivenessCounter)
+          if(doesBelong){
+            ctx.fillStyle = this.color.belongsColor;
+          } else {
+            ctx.fillStyle = this.color.failColor1;
+          }
+          ctx.fillRect(x,y,sizeCounter,sizeCounter)
+        }
+      }
+    }  
   }
 });
--- app/components/precision-slider.js
+++ app/components/precision-slider.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    onPrecisionChange(value) {
+      this.get('updateHandler')(value);
+      return value;
+    }
+  }
 });
