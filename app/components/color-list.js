78cb2ec0bb474a6f35f08b8c6bdb7457bcc6fbc8
blip <b@bradenlawrence.com>
2018-12-16 22:14:20 -0500

    Add color changer input

--- app/components/color-list.js
+++ app/components/color-list.js
@@ -1,4 +1,20 @@
 import Component from '@ember/component';
 
+const isHexColor = (input) => {
+  if(input.charAt(0) === '#' && input.length === 7){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export default Component.extend({
+  actions: {
+    onColorChange(value){
+      if(isHexColor(value)){
+        this.get('updateHandler')(value);
+      }
+      return value;
+    }
+  }
 });
