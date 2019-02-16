37b02fb4d9c1e9c8999889145b8e3c0b68c34443
blip <b@bradenlawrence.com>
2019-02-16 11:39:21 -0500

    Rename zoom-slider to zoom-controls

--- tests/integration/components/zoom-controls-test.js
+++ tests/integration/components/zoom-controls-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | zoom-slider', function(hooks) {
+module('Integration | Component | zoom-controls', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{zoom-slider}}`);
+    await render(hbs`{{zoom-controls}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#zoom-slider}}
+      {{#zoom-controls}}
         template block text
-      {{/zoom-slider}}
+      {{/zoom-controls}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
