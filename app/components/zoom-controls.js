dcbf47e61b7d259d2c4d8a28a2bf3a6f47df592f
blip <b@bradenlawrence.com>
2019-02-16 12:04:25 -0500

    Update zoom controls to use buttons instead of a slider

--- app/components/zoom-controls.js
+++ app/components/zoom-controls.js
@@ -3,6 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
   actions: {
     onZoomChange(value) {
+      this.position.zoom += value;
       this.get('updateHandler')(value);
     }
   }
